b11d07402ca388766bae118aad2ef44c4a4e428f
2013058686 <uhi1117@naver.com>
2018-03-01 21:49:17 +0900

    no message

---
+++ chapter01/src/chapter01/CheckPoint_08.java
@@ -0,0 +1,26 @@
+package chapter01;
+
+
+class Person{
+	private Car own;
+	
+
+	public void setOwn(Car own) {
+		this.own = own;
+	}
+	
+	public Car getOwn() {
+		return own;
+	}
+}
+class Car{
+	
+}
+public class CheckPoint_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

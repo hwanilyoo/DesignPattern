f0ef88edb95a7c550e6166b7ebc9f11816759081
2013058686 <uhi1117@naver.com>
2018-03-01 21:11:56 +0900

    no message

---
+++ chapter01/src/chapter01/CheckPoint_07.java
@@ -0,0 +1,61 @@
+package chapter01;
+
+abstract class HomeAppliancies{
+	private int serialNo;
+	private String manufacturer;
+	private int year;
+	
+	public abstract void turnOn();
+	
+	public abstract void turnOff(); 
+}
+class Washer extends HomeAppliancies{
+
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+class Freezer extends HomeAppliancies{
+	
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+class DishWasher extends HomeAppliancies{
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+public class CheckPoint_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
